3aa024388964557a7e59c1bb0968b490fe7f0560
admin <admin@lolejniczak.altostrat.com>
2023-02-22 19:10:45 +0000

    cpr

--- ccc_cpredictor/predictor.py
+++ ccc_cpredictor/predictor.py
@@ -6,6 +6,9 @@ import json
 from google.cloud.aiplatform.prediction.predictor import Predictor
 from google.cloud.aiplatform.utils import prediction_utils
 import tensorflow as tf
+from google.cloud import aiplatform
+from google.cloud.aiplatform import Featurestore
+import numpy as np
 
 class CustomPredictor(Predictor):
     def __init__(self):
@@ -25,9 +28,14 @@ class CustomPredictor(Predictor):
         
     def predict(self, instances):
         user_ids = instances["instances"]
-        instances = self._stats_entity_type.read(entity_ids=[user_ids]) ##get all features , feature_ids=_features)
-        inputs = instances.values.tolist()[:][1:]
-        outputs = self._model.predict([inputs])
+        print("user ids",user_ids)
+        instance_ids=list(np.concatenate(user_ids))
+        instances = self._stats_entity_type.read(entity_ids=instance_ids) ##get all features , feature_ids=_features)
+        inputs = instances.drop("entity_id", axis=1)
+        pinputs = inputs.values.tolist()
+
+        outputs = self._model.predict(pinputs)
+        print(outputs)
         return outputs.tolist()
         
     def postprocess(self, prediction_results: list):
